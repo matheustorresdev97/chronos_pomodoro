5c3156a22f39d73aaa4aec19c215f5c3ee383c1f
matheustorresdev97 <matheustorresdev@gmail.com>
2025-06-07 19:42:19 -0300

    feat: salvando o tema no localStorage

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,12 +1,21 @@
 import { useEffect, useState } from 'react';
 
 import styles from './styles.module.css';
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
+import { HistoryIcon, HouseIcon, MoonIcon, SettingsIcon, SunIcon } from 'lucide-react';
 
 type AvailableThemes = 'dark' | 'light';
 
 export function Menu() {
-  const [theme, setTheme] = useState<AvailableThemes>('dark');
+  const [theme, setTheme] = useState<AvailableThemes>(() => {
+    const storageTheme =
+      (localStorage.getItem('theme') as AvailableThemes) || 'dark';
+    return storageTheme;
+  });
+
+  const nextThemeIcon = {
+    dark: <SunIcon />,
+    light: <MoonIcon />,
+  };
 
   function handleThemeChange(
     event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
@@ -25,6 +34,7 @@ export function Menu() {
   useEffect(() => {
     console.log('Theme mudou', theme, Date.now());
     document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
   }, [theme]);
 
   return (
@@ -64,7 +74,7 @@ export function Menu() {
         title='Mudar Tema'
         onClick={handleThemeChange}
       >
-        <SunIcon />
+        {nextThemeIcon[theme]}
       </a>
     </nav>
   );
