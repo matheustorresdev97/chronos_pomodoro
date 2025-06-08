b7ee255ac9d55ff38a08a5be5965447e131cf5cc
matheustorresdev97 <matheustorresdev@gmail.com>
2025-06-08 14:05:09 -0300

    feat: state global da aplicação com useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
 import './styles/theme.css';
 import './styles/index.css';
 
+import { useState } from 'react';
+
 import { Home } from './pages/Home';
+import type { TaskStateModel } from './models/TaskStateModel';
+
+
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsRemaining: '00:00',
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+};
 
 export default function App() {
+  const [state, setState] = useState(initialState);
 
-  return <Home />;
+
+  return <Home state={state} setState={setState} />;
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,9 +3,17 @@ import { DefaultLayout } from '../../layouts/DefaultLayout';
 import { Container } from '../../components/Container';
 import { CountDown } from '../../components/CountDown';
 import { MainForm } from '../../components/MainForm';
+import type { TaskStateModel } from '../../models/TaskStateModel';
+
+type HomeProps = {
+  state: TaskStateModel;
+  setState: React.Dispatch<React.SetStateAction<TaskStateModel>>;
+};
 
 
-export function Home() {
+export function Home(props: HomeProps) {
+  const { state, setState } = props;
+
   return (
     <DefaultLayout>
       <Container>
