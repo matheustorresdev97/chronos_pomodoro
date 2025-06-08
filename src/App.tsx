c90f58836bdf90cccba54921d81524788c9ddc76
matheustorresdev97 <matheustorresdev@gmail.com>
2025-06-08 14:26:56 -0300

    fix: separando responsabilidades do contexto

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './styles/theme.css';
 import './styles/index.css';
 
 import { Home } from './pages/Home';
-import { TaskContextProvider } from './contexts/TaskContext';
+import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider';
 
 export default function App() {
   return (
