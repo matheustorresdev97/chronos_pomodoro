89ee44263e89634fe28d07a38dfd62b6b85d82fe
matheustorresdev97 <matheustorresdev@gmail.com>
2025-06-08 14:16:02 -0300

    fix: removendo props e criando contexto

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,12 @@
 import './styles/theme.css';
 import './styles/index.css';
 
-import { useState } from 'react';
-
 import { Home } from './pages/Home';
-import type { TaskStateModel } from './models/TaskStateModel';
-
-
-const initialState: TaskStateModel = {
-  tasks: [],
-  secondsRemaining: 0,
-  formattedSecondsRemaining: '00:00',
-  activeTask: null,
-  currentCycle: 0,
-  config: {
-    workTime: 25,
-    shortBreakTime: 5,
-    longBreakTime: 15,
-  },
-};
+import { TaskContextProvider } from './contexts/TaskContext';
 
 export default function App() {
-  const [state, setState] = useState(initialState);
 
-
-  return <Home state={state} setState={setState} />;
+  <TaskContextProvider>
+    <Home />
+  </TaskContextProvider>
 }
