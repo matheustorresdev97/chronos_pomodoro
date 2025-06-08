dd7190eff6184be37d9dc7e15e16e0cdef9f7094
matheustorresdev97 <matheustorresdev@gmail.com>
2025-06-08 14:28:34 -0300

    feat: input controlado

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,18 +1,30 @@
+import { useState } from 'react';
 import { PlayCircleIcon } from 'lucide-react';
 import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
 
 export function MainForm() {
+  const [taskName, setTaskName] = useState('');
+
+
+  function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    console.log('DEU CERTO', taskName);
+  }
 
   return (
-    <form className='form' action=''>
+    <form onSubmit={handleCreateNewTask} className='form' action=''>
+
       <div className='formRow'>
         <DefaultInput
           labelText='task'
           id='meuInput'
           type='text'
           placeholder='Digite algo'
+          value={taskName}
+          onChange={e => setTaskName(e.target.value)}
         />
       </div>
 
