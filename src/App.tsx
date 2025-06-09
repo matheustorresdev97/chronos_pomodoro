b3d9dfc6fc6f22fa59e033d171d7195523e6a2da
matheustorresdev97 <matheustorresdev@gmail.com>
2025-06-09 09:11:50 -0300

    feat: add lib React Toastify

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import './styles/index.css';
 
 import { Home } from './pages/Home';
 import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider';
+import { MessagesContainer } from './components/MessageContainer';
 
 export default function App() {
   return (
-  <TaskContextProvider>
-    <Home />
-  </TaskContextProvider>
+    <TaskContextProvider>
+      <MessagesContainer>
+        <Home />
+      </MessagesContainer>
+    </TaskContextProvider>
   )
 }
