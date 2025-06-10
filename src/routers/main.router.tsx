5f78ae4e454c52989b2268f8c59ba6aa23764cf8
matheustorresdev97 <matheustorresdev@gmail.com>
2025-06-10 11:31:15 -0300

    feat: page history

--- src/routers/main.router.tsx
+++ src/routers/main.router.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import { Home } from "../pages/Home";
 import { AboutPomodoro } from "../pages/AboutPomodoro";
 import { NotFound } from "../pages/NotFound";
+import History from "../pages/History";
 
 export function MainRouter() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Home />} />
+                <Route path='/history/' element={<History />} />
                 <Route path='/about-pomodoro/' element={<AboutPomodoro />} />
                 <Route path='*' element={<NotFound />} />
             </Routes>
